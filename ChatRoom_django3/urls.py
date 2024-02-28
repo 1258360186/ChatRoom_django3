16c0f4ceeb588f7917c0e705bfad3128d896ce92
baiyu <1258360186@qq.com>
2024-02-28 23:09:53 +0800

    first try REST_FRAMEWORK

--- ChatRoom_django3/urls.py
+++ ChatRoom_django3/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from myapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', views.first_api),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
