419010e1a874fcc1777df55e4c9cac1d632f2100
baiyu <1258360186@qq.com>
2024-02-28 22:33:16 +0800

    add MEDIA ROOT and URL

--- ChatRoom_django3/settings.py
+++ ChatRoom_django3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "myapp.apps.MyappConfig"
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'upload/'
+MEDIA_ROOT = BASE_DIR / 'upload'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
