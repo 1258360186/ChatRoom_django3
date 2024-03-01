d142a897ead976a97c818f21d089f9acc5c53302
baiyu <1258360186@qq.com>
2024-03-01 13:24:30 +0800

    create model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Userinfo(models.Model):
+
+    nickname = models.CharField(max_length=200,blank=True,null=True)
+    headImg = models.ImageField(upload_to='headImg/',blank=True,null=True)
+    belong = models.OneToOneField(User,on_delete=models.CASCADE,related_name='userinfo_user',blank=True,null=True)
+    def __int__(self):
+        return self.id
